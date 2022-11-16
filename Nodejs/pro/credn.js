df5e5cf29390614b176a827a64c593cc10988e74
Sejal kathane <kathanesejal@gmail.com>
2022-11-16 13:08:10 +0530

    add nodejs

---
+++ Nodejs/pro/credn.js
@@ -0,0 +1,5 @@
+const user='Nodejspro123@gmail.com';
+const pass= 'vdrengeucrbgkiip';
+
+exports.user=user;
+exports.pass=pass;
--- Nodejs/pro/index.js
+++ Nodejs/pro/index.js
@@ -1,127 +1,172 @@
-// var express=require("express");
-// var http=require("http");
-// var path =require("path");
-// var nodemailer=require("nodemailer");
 
 
-// //create connected express app
-// var app=express();
-// //create htttp server for our app
-// var server=http.server(app);
+// var express = require("express");
+// var http = require("http")
+// var path = require("path")
+// var nodemailer = require("nodemailer")
 
-// var port=500;
+// var app = express();
+// var server = http.Server(app)
+// var port = 587
 
-// app.set("port",port);
-// app.use(express.json());
-// app.use(express.urlencoded({extended:true}));
-
-// //join path string
-// app.use(express.static(path.join(__dirname,"static")));
+// app.set("port", port)
+// app.use(express.json())
+// app.use(express.urlencoded({extended:true}))
+// app.use(express.static(path.join(__dirname, "static")))
 
 // //Routing
-// app.get("/",function(req,responce){
-//     responce.sendfile(path.join(__dirname,"static/index.html"))
+// app.get("/", function(req, response){
+//     response.sendFile (path.join(__dirname, "static/index.html"))
 // })
 
-
-// app.post("/send_email",function(req,reaponse){
-//     var from=req.body.from;
-//     var to =req.body.to;
-//     var subject=req.body.subject;
-//     var message=req.body.message;
+// app.post("/send_email", function(req, response){
+//     var from = req.body.from
+//     var to = req.body.to
+//     var subject = req.body.subject
+//     var message = req.body.message
 
 //     var transporter = nodemailer.createTransport({
-//         service:'gmail',
-//         auth:{
-//             user :'Nodejspro123@gmail.com',
-//             pass: 'udigzvaabcpjqdhq'
+//         service: 'gmail',
+//         auth: {
+//             user: 'Nodejspro123@gmail.com',
+//             pass: 'vdrengeucrbgkiip'
 //         }
-//     });
+//     })
 
-//     var mailOptions={
-//         from:from,
+//     var mailOptions = {
+//         from: from,
 //         to:to,
 //         subject:subject,
 //         text:message
-//     };
+//     }
 
-//     transporter.sendMail(mailOptions,function(error,info){
-//         if(error){
+//     transporter.sendMail(mailOptions, function(error, info){
+//         if (error) {
 //             console.log(error)
+//         } else {
+//             console.log("Email Sent: " + info.response)
 //         }
-//         else{
-//             console.log("email Send: "+info.response)
-//         }
-//         responce.redirect("/")
+//         response.redirect("/")
 //     })
+// });
+
+// //Initialize Web Server
+// server.listen(port, function(){
+//     console.log("starting server on port " + port)
 // })
-
-
-// //INitilize web server
-// server.listen(port,function(){
-//     console.log("starting server on port"+port);
-// })
-
-
-
 // console.clear()
 
 
 
+////-----------------------------------------------------------
 
 
-var express = require("express");
-var http = require("http")
-var path = require("path")
-var nodemailer = require("nodemailer")
 
-var app = express();
-var server = http.Server(app)
-var port = 500
+// var nodemailer = require('nodemailer');
 
-app.set("port", port)
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
-app.use(express.static(path.join(__dirname, "static")))
+// var transporter = nodemailer.createTransport({
+//   service: 'gmail',
+//   auth: {
+//     user: 'Nodejspro123@gmail.com',
+//     pass: 'vdrengeucrbgkiip'
+//   }
+// });
 
-//Routing
-app.get("/", function(req, response){
-    response.sendFile(path.join(__dirname, "static/index.html"))
-})
+// var mailOptions = {
+//   from: 'Nodejspro123@gmail.com',
+//   to: 'kathanesejal@gmail.com',
+//   subject: 'HI',
+//   text: `I am NOdejs how are you`
+// };
 
-app.post("/send_email", function(req, response){
-    var from = req.body.from
-    var to = req.body.to
-    var subject = req.body.subject
-    var message = req.body.message
+// //function requires to send mail
+// transporter.sendMail(mailOptions, function(error, info){
+//   if (error) {
+//     console.log(error);
+//   } else {
+//     console.log('Email sent: ' + info.response);
+//   }
+// });
 
-    var transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            user :'Nodejspro123@gmail.com',
-            pass: 'udigzvaabcpjqdhq'
-        }
-    })
 
-    var mailOptions = {
-        from: from,
-        to:to,
-        subject:subject,
-        text:message
+
+
+
+//add express module 
+const express = require('express');
+const { dirname } = require('path');
+const nodemailer=require('nodemailer');
+
+const crd=require('./credn')
+const app=express();
+const port=3001;
+
+//axcess folder by express
+// app.use(express.static('foldername'));
+
+
+//routing through app object, get function
+
+
+app.get('/',(req,res)=>{
+    // res.send('server is happy..');
+    //get out path and index.html join them
+    res.sendFile(__dirname+'/index.html')
+});
+
+
+
+app.get('/send',(req,res)=>{
+    // res.send('hello server');
+    // res.send(req.query);
+
+    // fetching data from form 
+
+    let email1=req.query.email1;
+    let email2=req.query.email2;
+    let subject=req.query.subject;
+    let message=req.query.message;
+
+    
+const mail=nodemailer.createTransport({
+    host:'smtp.gmail.com' ,
+    port:587,
+    secure:false,
+    auth:
+    {
+        user:crd.user,
+        pass:crd.pass
     }
+});
+    mail.sendMail({
+        from:'Nodejspro123@gmail.com',
+        to:[email1,email2],
+        subject:subject,
+        html:message
+    },(err)=>{
+        if(err)
+          throw err;
+        res.send('Mail send')
+    });
 
-    transporter.sendMail(mailOptions, function(error, info){
-        if (error) {
-            console.log(error)
-        } else {
-            console.log("Email Sent: " + info.response)
-        }
-        response.redirect("/")
-    })
-})
+});
 
-//Initialize Web Server
-server.listen(port, function(){
-    console.log("starting server on port " + port)
-})
-console.clear()
+
+
+
+
+
+
+
+
+//call port server using nodejs
+app.listen(port,(err)=>{
+  if(err)
+     throw err;
+
+  console.log("Server is run ning at port %d ",port);
+
+
+
+  
+});
